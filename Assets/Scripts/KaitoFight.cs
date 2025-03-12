68c11b7bb99f9231bba3e853767bf36b727def59
DMichaY <101095899+DMichaY@users.noreply.github.com>
2025-03-12 09:23:11 +0100

    12/03/2025

--- Assets/Scripts/KaitoFight.cs
+++ Assets/Scripts/KaitoFight.cs
@@ -1,96 +1,97 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class KaitoFight : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    private Animator animator;
-    private Transform kaitoPivot;
-    private bool isAttacking = false;
-    
+    public float moveAmount = 1.0f; // Cantidad que se mueve en Z cuando se pulsa A o D
+    private Animator kaitoAnimator;
+    private Transform kaitoTransform;
+    private bool isActionInProgress = false;
+
     void Start()
     {
-        kaitoPivot = this.transform;
-        animator = kaitoPivot.GetChild(0).GetComponent<Animator>();
+        // Buscar el Animator en el hijo Kaito
+        kaitoAnimator = GetComponentInChildren<Animator>();
+        kaitoTransform = GetComponentInChildren<Transform>();
+
+        if (kaitoAnimator == null)
+        {
+            Debug.LogError("No se encontró un Animator en el hijo Kaito.");
+        }
     }
 
     void Update()
     {
-        if (!isAttacking)
+        if (isActionInProgress)
         {
-            HandleMovement();
+            // Si una animación está en progreso, no se permite otra acción
+            return;
         }
-        HandleAttacks();
-    }
 
-    void HandleMovement()
-    {
-        bool isMoving = false;
-        if (Input.GetKey(KeyCode.A))
+        // Comprobar las teclas y ejecutar las animaciones
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            kaitoPivot.position += Vector3.forward * moveSpeed * Time.deltaTime;
-            animator.SetBool("goLeft", true);
-            isMoving = true;
+            StartCoroutine(PerformAction("goLeft", moveAmount));
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            StartCoroutine(PerformAction("goRight", -moveAmount));
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            StartCoroutine(PerformAction("crouch"));
+        }
+        else if (Input.GetKeyDown(KeyCode.U))
+        {
+            StartCoroutine(PerformAction("punchLeft"));
+        }
+        else if (Input.GetKeyDown(KeyCode.I))
+        {
+            StartCoroutine(PerformAction("punchRight"));
+        }
+        else if (Input.GetKeyDown(KeyCode.J))
+        {
+            StartCoroutine(PerformAction("kickLeft"));
+        }
+        else if (Input.GetKeyDown(KeyCode.K))
+        {
+            StartCoroutine(PerformAction("kickRight"));
         }
         else
         {
-            animator.SetBool("goLeft", false);
-        }
-        
-        if (Input.GetKey(KeyCode.D))
-        {
-            kaitoPivot.position += Vector3.back * moveSpeed * Time.deltaTime;
-            animator.SetBool("goRight", true);
-            isMoving = true;
-        }
-        else
-        {
-            animator.SetBool("goRight", false);
-        }
-        
-        if (Input.GetKey(KeyCode.S))
-        {
-            animator.SetBool("crouch", true);
-            return; // Evita que se mueva o ataque mientras está agachado
-        }
-        else
-        {
-            animator.SetBool("crouch", false);
-        }
-
-        if (!isMoving && !isAttacking)
-        {
-            animator.Play("idleFight");
+            // Si no se pulsa nada, ejecutar la animación de idleFight
+            kaitoAnimator.SetTrigger("idleFight");
         }
     }
 
-    void HandleAttacks()
+    // Corrutina para manejar las acciones
+    private IEnumerator PerformAction(string animationTrigger, float movementZ = 0)
     {
-        if (Input.GetKeyDown(KeyCode.U))
+        // Activar el trigger de la animación
+        kaitoAnimator.SetTrigger(animationTrigger);
+
+        // Si hay movimiento, ajustar la posición en Z
+        if (movementZ != 0)
         {
-            StartCoroutine(PerformAttack("punchLeft"));
+            Vector3 newPosition = transform.position;
+            newPosition.z += movementZ;
+            transform.position = newPosition;
         }
-        if (Input.GetKeyDown(KeyCode.I))
-        {
-            StartCoroutine(PerformAttack("punchRight"));
-        }
-        if (Input.GetKeyDown(KeyCode.J))
-        {
-            StartCoroutine(PerformAttack("kickLeft"));
-        }
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            StartCoroutine(PerformAttack("kickRight"));
-        }
+
+        // Marcar que una acción está en progreso
+        isActionInProgress = true;
+
+        // Esperar hasta que termine la animación antes de permitir otra acción
+        yield return new WaitForSeconds(GetCurrentAnimationLength());
+
+        // Desbloquear las acciones
+        isActionInProgress = false;
     }
 
-    IEnumerator PerformAttack(string attackName)
+    // Obtener la duración de la animación actual
+    private float GetCurrentAnimationLength()
     {
-        isAttacking = true;
-        animator.SetBool(attackName, true);
-        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
-        animator.SetBool(attackName, false);
-        isAttacking = false;
+        AnimatorStateInfo stateInfo = kaitoAnimator.GetCurrentAnimatorStateInfo(0);
+        return stateInfo.length;
     }
 }
