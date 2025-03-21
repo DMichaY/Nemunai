f346d98ffb56a45421a3a74d355ab8182e4904f2
MidasNG <ngmidas09@gmail.com>
2025-03-21 09:44:21 +0100

    Mejora de transiciones en lucha

--- Assets/Scripts/KaitoFight.cs
+++ Assets/Scripts/KaitoFight.cs
@@ -4,17 +4,14 @@ using UnityEngine.InputSystem;
 
 public class KaitoFight : MonoBehaviour
 {
-    public float moveAmount = 1.0f; // Cantidad que se mueve en Z cuando se pulsa A o D
+    public float speed = 4.0f;
     private Animator kaitoAnimator;
-    private Transform kaitoTransform;
-    private bool isActionInProgress = false;
     private Vector2 movimiento;
 
     void Start()
     {
         // Buscar el Animator en el hijo Kaito
         kaitoAnimator = GetComponentInChildren<Animator>();
-        kaitoTransform = GetComponentInChildren<Transform>();
 
         if (kaitoAnimator == null)
         {
@@ -24,71 +21,7 @@ public class KaitoFight : MonoBehaviour
 
     void Update()
     {
-<<<<<<< Updated upstream
-        if (!kaitoAnimator.GetBool("crouch")) transform.position = transform.position + new Vector3(0, 0, movimiento.x * Time.deltaTime * 2);
-=======
-        /*
-        if (isActionInProgress)
-        {
-            // Si una animación está en progreso, no se permite otra acción
-            return;
-        }
-
-        // Comprobar las teclas y ejecutar las animaciones
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            StartCoroutine(PerformAction("goLeft", moveAmount));
-        }
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
-            StartCoroutine(PerformAction("goRight", -moveAmount));
-        }
-        else if (Input.GetKeyDown(KeyCode.S))
-        {
-            StartCoroutine(PerformAction("crouch"));
-        }
-        else if (Input.GetKeyDown(KeyCode.U))
-        {
-            StartCoroutine(PerformAction("punchLeft"));
-        }
-        else if (Input.GetKeyDown(KeyCode.I))
-        {
-            StartCoroutine(PerformAction("punchRight"));
-        }
-        else if (Input.GetKeyDown(KeyCode.J))
-        {
-            StartCoroutine(PerformAction("kickLeft"));
-        }
-        else if (Input.GetKeyDown(KeyCode.K))
-        {
-            StartCoroutine(PerformAction("kickRight"));
-        }
-        else
-        {
-            // Si no se pulsa nada, ejecutar la animación de idleFight
-            kaitoAnimator.SetTrigger("idleFight");
-        }
-        */
-        AplicarMovimiento();
-    }
-
-    public void OnMovimiento(InputValue value)
-    {
-        movimiento = value.Get<Vector2>();
-
-        if (movimiento.x > 0) kaitoAnimator.SetBool("goRight", true);
-        else kaitoAnimator.SetBool("goRight", false);
-        if (movimiento.x < 0) kaitoAnimator.SetBool("goLeft", true);
-        kaitoAnimator.SetBool("goLeft", false);
-
-    }
-
-    void AplicarMovimiento()
-    {
-        
-
-        transform.position = transform.position + new Vector3(0, 0, -movimiento.x * Time.deltaTime * 2);
->>>>>>> Stashed changes
+        if (!kaitoAnimator.GetBool("crouch")) transform.position = transform.position + new Vector3(0, 0, movimiento.x * Time.deltaTime * speed);
     }
 
     public void OnMovimiento(InputValue value)
@@ -102,17 +35,39 @@ public class KaitoFight : MonoBehaviour
         if (movimiento.y < 0) kaitoAnimator.SetBool("crouch", true);
         else kaitoAnimator.SetBool("crouch", false);
 
-        if (movimiento.magnitude == 0) kaitoAnimator.SetTrigger("idle");
-
     }
 
     public void OnAtacar(InputValue value)
     {
         Vector2 vectorAtaque = value.Get<Vector2>();
 
-        if (vectorAtaque.x > 0) kaitoAnimator.SetTrigger("kickRight");
-        else if (vectorAtaque.x < 0) kaitoAnimator.SetTrigger("punchLeft");
-        else if (vectorAtaque.y > 0) kaitoAnimator.SetTrigger("punchRight");
-        else if (vectorAtaque.y < 0) kaitoAnimator.SetTrigger("kickLeft");
+        if (vectorAtaque.x > 0)
+        {
+            kaitoAnimator.SetTrigger("kickRight");
+            kaitoAnimator.ResetTrigger("punchLeft");
+            kaitoAnimator.ResetTrigger("punchRight");
+            kaitoAnimator.ResetTrigger("kickLeft");
+        }
+        else if (vectorAtaque.x < 0)
+        {
+            kaitoAnimator.SetTrigger("punchLeft");
+            kaitoAnimator.ResetTrigger("kickRight");
+            kaitoAnimator.ResetTrigger("punchRight");
+            kaitoAnimator.ResetTrigger("kickLeft");
+        }
+        else if (vectorAtaque.y > 0)
+        {
+            kaitoAnimator.SetTrigger("punchRight");
+            kaitoAnimator.ResetTrigger("kickRight");
+            kaitoAnimator.ResetTrigger("punchLeft");
+            kaitoAnimator.ResetTrigger("kickLeft");
+        }
+        else if (vectorAtaque.y < 0)
+        {
+            kaitoAnimator.SetTrigger("kickLeft");
+            kaitoAnimator.ResetTrigger("kickRight");
+            kaitoAnimator.ResetTrigger("punchLeft");
+            kaitoAnimator.ResetTrigger("punchRight");
+        }
     }
 }
