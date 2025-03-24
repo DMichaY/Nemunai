2dea8f53e6737f962b008293df9e7258b033069b
GonzaloBeltranCatala <gbeltrancatala@gmail.com>
2025-03-24 09:53:28 +0100

    Funcionalidad Menu Principal
    Codigo de los principales botones del menu principal

---
+++ Assets/Scripts/Menus/MenuInicio.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuInicio : MonoBehaviour
+{
+    public GameObject menu;
+
+    public GameObject opciones;
+
+    public GameObject idiomas;
+
+
+    //---MENU INICIO---
+    //boton Jugar
+    public void Jugar()
+    {
+        //estacion
+        //SceneManager.LoadScene(escena estacion);
+
+    }
+
+    //boton opciones
+    public void Opciones()
+    {
+        menu.SetActive(false);
+
+        idiomas.SetActive(false);
+
+        opciones.SetActive(true);
+
+    }
+
+
+    //boton salir
+    public void Salir()
+    {
+        Application.Quit();
+    }
+
+
+    //boton ITCH.IO
+    public void Itch()
+    {
+        Application.OpenURL("https://itch.io/");
+    }
+
+    //---MENU OPCIONES---
+
+    //boton atras en el apartado de opciones
+    public void Idiomas()
+    {
+        opciones.SetActive(false);
+
+        menu.SetActive(false);
+
+        idiomas.SetActive(true);
+    }
+
+    public void AtrasOpciones()
+    {
+        opciones.SetActive(false);
+
+        idiomas.SetActive(false);
+
+        menu.SetActive(true);
+    }
+
+
+    //---MENU IDIOMAS---
+
+
+    //boton atras en el apartado de idiomas
+    public void AtrasIdiomas()
+    {
+        idiomas.SetActive(false);
+
+        menu.SetActive(false);
+
+        opciones.SetActive(true);
+    }
+
+}
