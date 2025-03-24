02cfe6926879f168cacb6b628fd91fb3b9c0fd18
GonzaloBeltranCatala <gbeltrancatala@gmail.com>
2025-03-24 11:43:31 +0100

    Blocking Pausa
    Menu basico de pausa con codigo funcional

---
+++ Assets/Scripts/Menus/MenuPausa.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPausa : MonoBehaviour
+{
+
+    public GameObject pausaMenu;
+
+    public bool pausado = false;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //menu de pausa
+        if ((Input.GetKeyDown(KeyCode.Escape)))
+        {
+            if (!pausado)
+            {
+                
+                Pausa();
+
+            }
+            else
+            {
+                Continuar();
+
+            }
+            
+
+        }
+    }
+    //pausar y mostrar menu
+    public void Pausa()
+    {
+        pausaMenu.SetActive(true);
+        Time.timeScale = 0f;
+        pausado = true;
+    }
+
+    //quita la pausa
+    public void Continuar()
+    {
+        pausaMenu.SetActive(false);
+        Time.timeScale = 1f;
+        pausado = false;
+
+    }
+
+
+    //boton salir
+    public void Salir()
+    {
+        Application.Quit();
+    }
+
+
+    //volver al menu
+    public void VolverMenu()
+    {
+        //menu
+        //SceneManager.LoadScene(escena menu inicio);
+
+    }
+
+
+}
