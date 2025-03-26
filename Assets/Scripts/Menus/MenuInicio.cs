b1c1945f572618f4fc9718b9bcff7bcda5d547fd
GonzaloBeltranCatala <gbeltrancatala@gmail.com>
2025-03-26 08:56:01 +0100

    Traducciones menus
    Arreglar que las traducciones no se muestren en elos menus de estacion y pueblo

--- Assets/Scripts/Menus/MenuInicio.cs
+++ Assets/Scripts/Menus/MenuInicio.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuInicio : MonoBehaviour
 {
@@ -16,7 +17,7 @@ public class MenuInicio : MonoBehaviour
     public void Jugar()
     {
         //estacion
-        //SceneManager.LoadScene(escena estacion);
+        SceneManager.LoadScene("Blockout Estacion");
 
     }
 
