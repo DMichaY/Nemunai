8effcf1206c8d7492b943688462adc1d1d16090a
Micha <davidmichavilayan4@gmail.com>
2025-05-04 23:52:34 +0200

    Menu diseño definitivo 👍
    Estoy cansado jefe

--- Assets/Scripts/Menus/MenuManager.cs
+++ Assets/Scripts/Menus/MenuManager.cs
@@ -1,167 +1,327 @@
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 
 public class MenuManager : MonoBehaviour
 {
-    [Header("DuraciÛn del movimiento de la c·mara")]
-    public float duracionMovimientoCamara = 1f;
-
-    [Header("Referencias de la c·mara y sus puntos objetivo")]
+    [Header("C√°mara")]
     public Transform camara;
-    public Transform camaraMenu;
-    public Transform camaraOpciones;
-    public Transform camaraCreditos;
-    public Transform camaraOpcionSeleccionada; // Compartido por vÌdeo, sonido, etc.
+    public float velocidadMovimiento = 0.5f;
 
-    [Header("Apartados del Canvas")]
-    public GameObject menuPrincipal;
-    public GameObject opciones;
-    public GameObject creditos;
-    public GameObject opcionesVideo;
-    public GameObject opcionesSonido;
-    public GameObject opcionesIdiomas;
-    public GameObject opcionesControles;
-
-    [Header("Im·genes que se desactivan al pasar el cursor")]
-    public GameObject imagenJugar;
-    public GameObject imagenItchio;
-    public GameObject imagenSalir;
-    public GameObject imagenOpciones;
-    public GameObject imagenCreditos;
-    public GameObject imagenVideo;
-    public GameObject imagenSonido;
-    public GameObject imagenIdiomas;
-    public GameObject imagenControles;
-
-    [Header("Escena a cargar al pulsar Jugar")]
-    public string nombreEscenaJugar;
-
-    [Header("Enlace de Itchio")]
-    public string urlItchio;
-
-    void Start()
+    [System.Serializable]
+    public class BotonConfiguracion
     {
-        // Aseguramos que la c·mara empiece en el punto correcto
-        camara.position = camaraMenu.position;
+        public string nombre;
+        public Button boton;
+        public GameObject imagenADesactivar;
+        public TipoAccion accion;
+        public string datoAccion;
+        public Transform destinoCamara;
     }
 
-    #region MÈtodos P˙blicos - Se asignan desde los botones
-
-    public void Jugar()
+    [System.Serializable]
+    public class BotonSimpleConfiguracion
     {
-        SceneManager.LoadScene(nombreEscenaJugar);
+        public Button boton;
+        public GameObject imagenADesactivar;
     }
 
-    public void IrAItchio()
+    public enum TipoAccion
     {
-        Application.OpenURL(urlItchio);
+        IrAEscena,
+        AbrirURL,
+        MoverCamara,
+        Salir
     }
 
-    public void SalirDelJuego()
+    [Header("Botones del Men√∫ Principal")]
+    public List<BotonConfiguracion> botonesMenu;
+
+    [Header("Botones del Men√∫ Opciones")]
+    public List<BotonConfiguracion> botonesOpciones;
+
+    [Header("Volver Atr√°s en Subapartados")]
+    public BotonConfiguracion botonVolverVideo;
+    public BotonConfiguracion botonVolverSonido;
+    public BotonConfiguracion botonVolverIdiomas;
+    public BotonConfiguracion botonVolverControles;
+    public BotonConfiguracion botonVolverCreditos;
+
+    [Header("Botones de Izquierda y Derecha con imagen a desactivar")]
+    public List<BotonSimpleConfiguracion> botonesSimples;
+
+    [Header("Pantalla")]
+    public Button botonPantallaIzquierda;
+    public Button botonPantallaDerecha;
+    public GameObject[] textosPantalla;
+    private int indexPantalla = 0;
+
+    [Header("Resoluci√≥n")]
+    public Button botonResolucionIzquierda;
+    public Button botonResolucionDerecha;
+    public GameObject[] textosResolucion;
+    private int indexResolucion = 1;
+
+    [Header("Calidad Gr√°fica")]
+    public Button botonGraficaIzquierda;
+    public Button botonGraficaDerecha;
+    public GameObject[] textosGrafica;
+    private int indexGrafica = 2;
+
+    [Header("Sliders de Sonido")]
+    public Slider sliderGeneral;
+    public Slider sliderMusica;
+    public Slider sliderSFX;
+
+    [Header("Idiomas")]
+    public Button botonEspa√±ol;
+    public Button botonRuso;
+    public Button botonIngles;
+    public Button botonJapones;
+    public GameObject imagenEspa√±ol;
+    public GameObject imagenRuso;
+    public GameObject imagenIngles;
+    public GameObject imagenJapones;
+
+    [Header("Controles")]
+    public Button botonTeclado;
+    public Button botonMando;
+    public GameObject imagenTeclado;
+    public GameObject imagenMando;
+
+    [Header("Cr√©ditos")]
+    public TextMeshProUGUI textoCreditos;
+    public float duracionEscrituraCreditos = 0.05f;
+    private string textoCreditosCompleto = "Cr√©ditos del Juego: Desarrollador: [Nombre]. M√∫sica por: [M√∫sico].";
+
+    private void Start()
     {
-        Application.Quit();
+        ConfigurarBotones(botonesMenu);
+        ConfigurarBotones(botonesOpciones);
+        ConfigurarBoton(botonVolverVideo);
+        ConfigurarBoton(botonVolverSonido);
+        ConfigurarBoton(botonVolverIdiomas);
+        ConfigurarBoton(botonVolverControles);
+        ConfigurarBoton(botonVolverCreditos);
+        ConfigurarBotonesSimples(botonesSimples);
+
+        // Configurar botones de idioma
+        ConfigurarBotonIdioma(botonEspa√±ol, imagenEspa√±ol);
+        ConfigurarBotonIdioma(botonRuso, imagenRuso);
+        ConfigurarBotonIdioma(botonIngles, imagenIngles);
+        ConfigurarBotonIdioma(botonJapones, imagenJapones);
+
+        botonPantallaIzquierda.onClick.AddListener(() => CambiarPantalla(-1));
+        botonPantallaDerecha.onClick.AddListener(() => CambiarPantalla(1));
+        botonResolucionIzquierda.onClick.AddListener(() => CambiarResolucion(-1));
+        botonResolucionDerecha.onClick.AddListener(() => CambiarResolucion(1));
+        botonGraficaIzquierda.onClick.AddListener(() => CambiarGrafica(-1));
+        botonGraficaDerecha.onClick.AddListener(() => CambiarGrafica(1));
+
+        ActualizarPantalla();
+        ActualizarResolucion();
+        ActualizarGrafica();
+
+        sliderGeneral.onValueChanged.AddListener((v) => PlayerPrefs.SetFloat("VolumenGeneral", v));
+        sliderMusica.onValueChanged.AddListener((v) => PlayerPrefs.SetFloat("VolumenMusica", v));
+        sliderSFX.onValueChanged.AddListener((v) => PlayerPrefs.SetFloat("VolumenSFX", v));
+
+        sliderGeneral.value = PlayerPrefs.GetFloat("VolumenGeneral", 1);
+        sliderMusica.value = PlayerPrefs.GetFloat("VolumenMusica", 1);
+        sliderSFX.value = PlayerPrefs.GetFloat("VolumenSFX", 1);
     }
 
-    public void IrAOpciones()
+    void ConfigurarBoton(BotonConfiguracion config)
     {
-        menuPrincipal.SetActive(false);
-        opciones.SetActive(true);
-        StartCoroutine(MoverCamara(camaraOpciones.position));
+        if (config.boton == null) return;
+
+        EventTrigger trigger = config.boton.GetComponent<EventTrigger>() ?? config.boton.gameObject.AddComponent<EventTrigger>();
+
+        var enter = new EventTrigger.Entry { eventID = EventTriggerType.PointerEnter };
+        enter.callback.AddListener((eventData) =>
+        {
+            if (config.imagenADesactivar != null)
+                config.imagenADesactivar.SetActive(false);
+        });
+        trigger.triggers.Add(enter);
+
+        var exit = new EventTrigger.Entry { eventID = EventTriggerType.PointerExit };
+        exit.callback.AddListener((eventData) =>
+        {
+            if (config.imagenADesactivar != null)
+                config.imagenADesactivar.SetActive(true);
+        });
+        trigger.triggers.Add(exit);
+
+        config.boton.onClick.AddListener(() =>
+        {
+            EjecutarAccion(config);
+
+            if (config.nombre.ToLower().Contains("creditos"))
+            {
+                StartCoroutine(AnimarTextoCreditos());
+            }
+        });
     }
 
-    public void IrACreditos()
+    void ConfigurarBotones(List<BotonConfiguracion> botones)
     {
-        menuPrincipal.SetActive(false);
-        creditos.SetActive(true);
-        StartCoroutine(MoverCamara(camaraCreditos.position));
+        foreach (var config in botones)
+            ConfigurarBoton(config);
     }
 
-    public void IrAVideo()
+    void ConfigurarBotonesSimples(List<BotonSimpleConfiguracion> botones)
     {
-        opciones.SetActive(false);
-        opcionesVideo.SetActive(true);
-        StartCoroutine(MoverCamara(camaraOpcionSeleccionada.position));
+        foreach (var simple in botones)
+        {
+            if (simple.boton == null) continue;
+
+            EventTrigger trigger = simple.boton.GetComponent<EventTrigger>() ?? simple.boton.gameObject.AddComponent<EventTrigger>();
+
+            var enter = new EventTrigger.Entry { eventID = EventTriggerType.PointerEnter };
+            enter.callback.AddListener((eventData) =>
+            {
+                if (simple.imagenADesactivar != null)
+                    simple.imagenADesactivar.SetActive(false);
+            });
+            trigger.triggers.Add(enter);
+
+            var exit = new EventTrigger.Entry { eventID = EventTriggerType.PointerExit };
+            exit.callback.AddListener((eventData) =>
+            {
+                if (simple.imagenADesactivar != null)
+                    simple.imagenADesactivar.SetActive(true);
+            });
+            trigger.triggers.Add(exit);
+        }
     }
 
-    public void IrASonido()
+    void ConfigurarBotonIdioma(Button boton, GameObject imagen)
     {
-        opciones.SetActive(false);
-        opcionesSonido.SetActive(true);
-        StartCoroutine(MoverCamara(camaraOpcionSeleccionada.position));
+        EventTrigger trigger = boton.GetComponent<EventTrigger>() ?? boton.gameObject.AddComponent<EventTrigger>();
+
+        var enter = new EventTrigger.Entry { eventID = EventTriggerType.PointerEnter };
+        enter.callback.AddListener((eventData) => { if (imagen != null) imagen.SetActive(false); });
+        trigger.triggers.Add(enter);
+
+        var exit = new EventTrigger.Entry { eventID = EventTriggerType.PointerExit };
+        exit.callback.AddListener((eventData) => { if (imagen != null) imagen.SetActive(true); });
+        trigger.triggers.Add(exit);
+
+        boton.onClick.AddListener(() => CambiarIdioma(imagen));
     }
 
-    public void IrAIdiomas()
+    void CambiarIdioma(GameObject imagen)
     {
-        opciones.SetActive(false);
-        opcionesIdiomas.SetActive(true);
-        StartCoroutine(MoverCamara(camaraOpcionSeleccionada.position));
+        if (imagen == null) return;
+
+        imagenEspa√±ol.SetActive(imagenEspa√±ol == imagen);
+        imagenRuso.SetActive(imagenRuso == imagen);
+        imagenIngles.SetActive(imagenIngles == imagen);
+        imagenJapones.SetActive(imagenJapones == imagen);
     }
 
-    public void IrAControles()
+    void EjecutarAccion(BotonConfiguracion config)
     {
-        opciones.SetActive(false);
-        opcionesControles.SetActive(true);
-        StartCoroutine(MoverCamara(camaraOpcionSeleccionada.position));
+        switch (config.accion)
+        {
+            case TipoAccion.IrAEscena:
+                if (!string.IsNullOrEmpty(config.datoAccion))
+                    SceneManager.LoadScene(config.datoAccion);
+                break;
+            case TipoAccion.AbrirURL:
+                if (!string.IsNullOrEmpty(config.datoAccion))
+                    Application.OpenURL(config.datoAccion);
+                break;
+            case TipoAccion.MoverCamara:
+                if (config.destinoCamara != null)
+                    StartCoroutine(MoverCamara(config.destinoCamara.position));
+                break;
+            case TipoAccion.Salir:
+                Application.Quit();
+                break;
+        }
     }
 
-    public void VolverAlMenuDesdeOpciones()
+    IEnumerator MoverCamara(Vector3 destino)
     {
-        opciones.SetActive(false);
-        menuPrincipal.SetActive(true);
-        StartCoroutine(MoverCamara(camaraMenu.position));
-    }
-
-    public void VolverAOpcionesDesdeSubmenu()
-    {
-        opcionesVideo.SetActive(false);
-        opcionesSonido.SetActive(false);
-        opcionesIdiomas.SetActive(false);
-        opcionesControles.SetActive(false);
-        opciones.SetActive(true);
-        StartCoroutine(MoverCamara(camaraOpciones.position));
-    }
-
-    public void VolverAlMenuDesdeCreditos()
-    {
-        creditos.SetActive(false);
-        menuPrincipal.SetActive(true);
-        StartCoroutine(MoverCamara(camaraMenu.position));
-    }
-
-    #endregion
-
-    #region MÈtodos para eventos del cursor
-
-    public void DesactivarImagen(GameObject imagen)
-    {
-        if (imagen != null) imagen.SetActive(false);
-    }
-
-    public void ActivarImagen(GameObject imagen)
-    {
-        if (imagen != null) imagen.SetActive(true);
-    }
-
-    #endregion
-
-    #region Movimiento suave de c·mara
-
-    private System.Collections.IEnumerator MoverCamara(Vector3 destino)
-    {
-        Vector3 inicio = camara.position;
+        Vector3 origen = camara.position;
         float tiempo = 0f;
 
-        while (tiempo < duracionMovimientoCamara)
+        while (tiempo < 1f)
         {
-            camara.position = Vector3.Lerp(inicio, destino, tiempo / duracionMovimientoCamara);
-            tiempo += Time.deltaTime;
+            tiempo += Time.deltaTime / velocidadMovimiento;
+            camara.position = Vector3.Lerp(origen, destino, tiempo);
             yield return null;
         }
 
         camara.position = destino;
     }
 
-    #endregion
+    IEnumerator AnimarTextoCreditos()
+    {
+        textoCreditos.text = "";
+        for (int i = 0; i <= textoCreditosCompleto.Length; i++)
+        {
+            textoCreditos.text = textoCreditosCompleto.Substring(0, i);
+            yield return new WaitForSeconds(duracionEscrituraCreditos);
+        }
+    }
+
+    void CambiarPantalla(int direccion)
+    {
+        indexPantalla = (indexPantalla + direccion + textosPantalla.Length) % textosPantalla.Length;
+        ActualizarPantalla();
+    }
+
+    void ActualizarPantalla()
+    {
+        for (int i = 0; i < textosPantalla.Length; i++)
+            textosPantalla[i].SetActive(i == indexPantalla);
+
+        switch (indexPantalla)
+        {
+            case 0: Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen; break;
+            case 1: Screen.fullScreenMode = FullScreenMode.Windowed; break;
+            case 2: Screen.fullScreenMode = FullScreenMode.FullScreenWindow; break;
+        }
+    }
+
+    void CambiarResolucion(int direccion)
+    {
+        indexResolucion = (indexResolucion + direccion + textosResolucion.Length) % textosResolucion.Length;
+        ActualizarResolucion();
+    }
+
+    void ActualizarResolucion()
+    {
+        for (int i = 0; i < textosResolucion.Length; i++)
+            textosResolucion[i].SetActive(i == indexResolucion);
+
+        switch (indexResolucion)
+        {
+            case 0: Screen.SetResolution(1280, 720, Screen.fullScreenMode); break;
+            case 1: Screen.SetResolution(1920, 1080, Screen.fullScreenMode); break;
+            case 2: Screen.SetResolution(2560, 1440, Screen.fullScreenMode); break;
+            case 3: Screen.SetResolution(3840, 2160, Screen.fullScreenMode); break;
+        }
+    }
+
+    void CambiarGrafica(int direccion)
+    {
+        indexGrafica = (indexGrafica + direccion + textosGrafica.Length) % textosGrafica.Length;
+        ActualizarGrafica();
+    }
+
+    void ActualizarGrafica()
+    {
+        for (int i = 0; i < textosGrafica.Length; i++)
+            textosGrafica[i].SetActive(i == indexGrafica);
+
+        QualitySettings.SetQualityLevel(indexGrafica);
+    }
 }
