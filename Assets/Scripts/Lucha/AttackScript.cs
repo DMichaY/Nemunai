996895ef9ec6fddcca4d80eba7a5c8a30e3f5ca0
MidasNG <ngmidas09@gmail.com>
2025-05-08 15:18:14 +0200

    Efectos de golpe

--- Assets/Scripts/Lucha/AttackScript.cs
+++ Assets/Scripts/Lucha/AttackScript.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class AttackScript : MonoBehaviour
 {
     public float damage = 5;
+    private static GameObject hitEffect;
+
+    private void Start()
+    {
+        if (hitEffect == null) hitEffect = GameObject.Find("HitEffect");
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Fighter") other.GetComponent<FighterClass>().GetHit(damage);
+        if (other.tag == "Fighter")
+        {
+            other.GetComponent<FighterClass>().GetHit(damage);
+            Destroy(Instantiate(hitEffect, other.ClosestPoint(transform.position) + new Vector3(2, 0, 0), Quaternion.identity), 1);
+        }
     }
 }
