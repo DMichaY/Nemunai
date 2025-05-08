be85d27f5f4e58c87b000e9eb761650a3152fb91
Miguel6GG <miggargar6@alu.edu.gva.es>
2025-05-08 22:27:20 +0200

    Walking Dead audios
    AHORA EL PUEBLO LUEGO DE REIKO ESTA LLENO DE GENTE POSEÍDA QUE SUENAN A ZOMBIES A A A A A A AH

--- Assets/Scripts/Sonidos/SonidosZombies.cs
+++ Assets/Scripts/Sonidos/SonidosZombies.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class SonidosZombies : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Variables
+    public AudioClip sonidoZombi1;
+    public AudioClip sonidoZombi2;
+    public AudioClip sonidoZombi3;
+    public AudioClip sonidoZombi4;
+    public AudioClip sonidoZombi5;
+    AudioSource audioFuente;
+
+    private List<AudioClip> listaSonidosZombi = new List<AudioClip>();
+
+    public float intervaloMin = 1f;
+    public float intervaloMax = 5f;
+
     void Start()
     {
-        
+        // Llenar la lista de sonidos
+        listaSonidosZombi.Add(sonidoZombi1);
+        listaSonidosZombi.Add(sonidoZombi2);
+        listaSonidosZombi.Add(sonidoZombi3);
+        listaSonidosZombi.Add(sonidoZombi4);
+        listaSonidosZombi.Add(sonidoZombi5);
+
+        audioFuente = this.GetComponent<AudioSource>();
+
+        // Iniciar la corrutina
+        StartCoroutine(SonidoZombiAleatorio());
     }
 
-    // Update is called once per frame
-    void Update()
+    // Esta corrutina inicia un clip aleatorio de zombie cada ciertos segundos (valor aleatorio de 1 a 5)
+    IEnumerator SonidoZombiAleatorio()
     {
-        
+        while (true)
+        {
+            // Rango aleatorio de tiempo mínimo y máximo
+            float tiempoEspera = Random.Range(intervaloMin, intervaloMax);
+            yield return new WaitForSeconds(tiempoEspera);
+
+            // Se escoge un sonido aleatorio de la lista
+            int indice = Random.Range(0, listaSonidosZombi.Count);
+            AudioClip sonidoSeleccionado = listaSonidosZombi[indice];
+
+            // Reproducir el sonido
+            audioFuente.PlayOneShot(sonidoSeleccionado);
+        }
     }
 }
