391e5bf2ee12aea0677f20faac254cc654e63428
Miguel García <151826401+Miguel6GG@users.noreply.github.com>
2025-05-09 08:58:28 +0200

    Audio Mixer funcional Menu Pausa
    Ahora el AudioMixer funciona en el menu de pausa dentro de escenas del juego

--- Assets/Scripts/Menus/MenuPausa.cs
+++ Assets/Scripts/Menus/MenuPausa.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.Audio;
 
 public class MenuPausa : MonoBehaviour
 {
@@ -50,10 +51,36 @@ public class MenuPausa : MonoBehaviour
     public GameObject imgBtnAtrasSonido;
     public Slider sliderGeneral, sliderMusica, sliderSFX;
 
+    [Header("Clips de audio")]
+    public AudioClip sonidoHover;
+    public AudioClip sonidoClick;
+
+    [Header("Mixer de audio y grupos")]
+    public AudioMixer mezcladorPrincipal;
+    public AudioMixerGroup grupoMusica;
+    public AudioMixerGroup grupoSFX;
+
+    private AudioSource fuenteSFX;
+
     [Header("Opciones Controles")]
     public Button btnAtrasControles, btnTeclado, btnMando;
     public GameObject imgBtnAtrasControles, imgBtnTeclado, imgBtnMando;
     public GameObject imagenControlesTeclado, imagenControlesMando;
+
+    void Awake()
+    {
+        // Crear AudioSource efectos
+        GameObject objetoSFX = new GameObject("FuenteAudioSFX");
+        objetoSFX.transform.parent = this.transform;
+        fuenteSFX = objetoSFX.AddComponent<AudioSource>();
+        fuenteSFX.outputAudioMixerGroup = grupoSFX;
+
+        // Asignar eventos de audio a todos los botones en la escena
+        foreach (Button boton in FindObjectsOfType<Button>())
+        {
+            AsignarEventosAudio(boton);
+        }
+    }
 
     void Start()
     {
@@ -227,5 +254,29 @@ public class MenuPausa : MonoBehaviour
     {
         imagenControlesTeclado.SetActive(teclado);
         imagenControlesMando.SetActive(!teclado);
+    }
+
+    void AsignarEventosAudio(Button boton)
+    {
+        EventTrigger disparador = boton.gameObject.GetComponent<EventTrigger>();
+        if (disparador == null)
+            disparador = boton.gameObject.AddComponent<EventTrigger>();
+
+        // Evento al pasar el cursor (hover)
+        EventTrigger.Entry entradaHover = new EventTrigger.Entry();
+        entradaHover.eventID = EventTriggerType.PointerEnter;
+        entradaHover.callback.AddListener((eventData) => ReproducirSFX(sonidoHover));
+        disparador.triggers.Add(entradaHover);
+
+        // Evento al hacer clic
+        boton.onClick.AddListener(() => ReproducirSFX(sonidoClick));
+    }
+
+    void ReproducirSFX(AudioClip clip)
+    {
+        if (clip != null && fuenteSFX != null)
+        {
+            fuenteSFX.PlayOneShot(clip);
+        }
     }
 }
