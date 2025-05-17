85e53222726568653dd415c9ad3e7993e6df2c8a
Miguel García <151826401+Miguel6GG@users.noreply.github.com>
2025-05-17 12:41:17 +0200

    Audios Poseido Golpeado
    Se han añadido e implementado los audios de los poseidos al ser golpeados.

--- Assets/Scripts/Lucha/Enemigos/EnemyFightAI.cs
+++ Assets/Scripts/Lucha/Enemigos/EnemyFightAI.cs
@@ -12,6 +12,20 @@ public class EnemyFightAI : FighterClass
     public Animator animator;
     public Rigidbody rb;
 
+    public AudioClip sonidoHITPos1;
+    public AudioClip sonidoHITPos2;
+    public AudioClip sonidoHITPos3;
+    public AudioClip sonidoHITPos4;
+    public AudioClip sonidoHITPos5;
+    public AudioClip sonidoHITPos6;
+    public AudioClip sonidoHITPos7;
+    public AudioClip sonidoHITPos8;
+    AudioSource audioFuente;
+
+    public GameObject cuerpoRefAudio;
+
+    private List<AudioClip> listaSonidosPos = new List<AudioClip>();
+
     //Estad�sticas
     public float speed = 6, waitTime = 3, attackTime = 1, life = 100, startWaitTime = 0;
 
@@ -28,6 +42,19 @@ public class EnemyFightAI : FighterClass
         lifeBar.value = life;
 
         StartCoroutine(StartWait());
+
+        // Audio
+        // Llenar la lista de sonidos
+        listaSonidosPos.Add(sonidoHITPos1);
+        listaSonidosPos.Add(sonidoHITPos2);
+        listaSonidosPos.Add(sonidoHITPos3);
+        listaSonidosPos.Add(sonidoHITPos4);
+        listaSonidosPos.Add(sonidoHITPos5);
+        listaSonidosPos.Add(sonidoHITPos6);
+        listaSonidosPos.Add(sonidoHITPos7);
+        listaSonidosPos.Add(sonidoHITPos8);
+
+        audioFuente = cuerpoRefAudio.GetComponent<AudioSource>();
     }
 
     private IEnumerator StartWait()
@@ -48,6 +75,7 @@ public class EnemyFightAI : FighterClass
         life -= damage;
         lifeBar.value = life;
 
+
         if (life <= 0 && !animator.GetBool("death"))
         {
             StartCoroutine("Death");
@@ -64,6 +92,8 @@ public class EnemyFightAI : FighterClass
                 HB.SetActive(false);
             }
         }
+
+        SonidoHITPosAleatorio();
     }
 
     private IEnumerator Death()
@@ -91,5 +121,16 @@ public class EnemyFightAI : FighterClass
     private void OnCollisionExit(Collision collision)
     {
         rb.velocity = Vector3.zero;
+    }
+
+    // Corrutina que produce sonido aleatorio del enemigo siendo golpeando
+    public void SonidoHITPosAleatorio()
+    {
+        // Se escoge un sonido aleatorio de la lista
+        int indice = Random.Range(0, listaSonidosPos.Count);
+        AudioClip sonidoSeleccionado = listaSonidosPos[indice];
+
+        // Reproducir el sonido
+        audioFuente.PlayOneShot(sonidoSeleccionado);
     }
 }
