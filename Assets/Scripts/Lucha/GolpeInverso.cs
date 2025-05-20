cf14b75c9db74da8fee7f139440f5f263974235d
GonzaloBeltranCatala <gbeltrancatala@gmail.com>
2025-05-20 10:22:53 +0200

    efecto peleas invertidas

--- Assets/Scripts/Lucha/GolpeInverso.cs
+++ Assets/Scripts/Lucha/GolpeInverso.cs
@@ -6,11 +6,18 @@ using UnityEngine.UI;
 public class GolpeInverso : MonoBehaviour
 {
 
-    public GameObject espiral;
+    public GameObject espiral1;
+
+    public GameObject espiral2;
+
+    public GameObject espiral3;
+
+    public GameObject espiral4;
 
 
 
-  
+
+
     public void RecibirGolpeInverso()
     {
 
@@ -18,39 +25,52 @@ public class GolpeInverso : MonoBehaviour
 
         IEnumerator Golpe()
         {
-            
-            if (espiral != null)
-            {
-                espiral.GetComponent<Image>().CrossFadeAlpha(0, 2, false);
 
-                espiral.GetComponent<Image>().color = new Color(255f, 255f, 255f, 5f);
+            espiral1.GetComponent<Image>().CrossFadeAlpha(0, 2, false);
 
-            }
+            espiral1.GetComponent<Image>().color = new Color(255f, 255f, 255f, 5f);
 
-            else
-            {
-                Debug.LogWarning("No hay pantalla negra!");
-            }
+
+            espiral2.GetComponent<Image>().CrossFadeAlpha(0, 2, false);
+
+            espiral2.GetComponent<Image>().color = new Color(255f, 255f, 255f, 5f);
+
+
+            espiral3.GetComponent<Image>().CrossFadeAlpha(0, 2, false);
+
+            espiral3.GetComponent<Image>().color = new Color(255f, 255f, 255f, 5f);
+
+
+            espiral4.GetComponent<Image>().CrossFadeAlpha(0, 2, false);
+
+            espiral4.GetComponent<Image>().color = new Color(255f, 255f, 255f, 5f);
+
 
             yield return new WaitForSeconds(2f);
 
-            espiral.GetComponent<Image>().color = new Color(255f,255f,255f,0f);
+            espiral1.GetComponent<Image>().color = new Color(255f,255f,255f,0f);
+
+            espiral2.GetComponent<Image>().color = new Color(255f, 255f, 255f, 0f);
+
+            espiral3.GetComponent<Image>().color = new Color(255f, 255f, 255f, 0f);
+
+            espiral4.GetComponent<Image>().color = new Color(255f, 255f, 255f, 0f);
 
 
             yield return new WaitForSeconds(0.2f);
 
-            if (espiral != null)
-            {
-                espiral.GetComponent<Image>().CrossFadeAlpha(1, 2, false);
+            espiral1.GetComponent<Image>().CrossFadeAlpha(1, 2, false);
 
-            }
+            espiral2.GetComponent<Image>().CrossFadeAlpha(1, 2, false);
 
-            else
-            {
-                Debug.LogWarning("No hay pantalla negra!");
-            }
+            espiral3.GetComponent<Image>().CrossFadeAlpha(1, 2, false);
 
-            
+            espiral4.GetComponent<Image>().CrossFadeAlpha(1, 2, false);
+
+
+
+
+
 
         }
        
