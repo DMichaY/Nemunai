7167729952f9a7f6b1762cc80c2e5af6d3c7b57e
GonzaloBeltranCatala <gbeltrancatala@gmail.com>
2025-05-20 12:02:53 +0200

    torres

---
+++ Assets/Scripts/CambioZonas/TorreTeleport.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TorreTeleport : MonoBehaviour
+{
+
+    public string nombreEscena;
+    public string tagJugador = "Player";
+    public Image pantallaNegra;
+
+    private void Start()
+    {
+        if (pantallaNegra != null) pantallaNegra.CrossFadeAlpha(0, 2, false);
+        else Debug.LogWarning("No hay pantalla negra!");
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+            if (other.CompareTag(tagJugador))
+            {
+                if (!string.IsNullOrEmpty(nombreEscena))
+                {
+                    StartCoroutine("SceneLoaderCoroutine");
+                }
+            }
+
+        }
+    }
+
+
+    public void ForceLoadScene()
+    {
+        StartCoroutine("SceneLoaderCoroutine");
+    }
+
+
+
+    private IEnumerator SceneLoaderCoroutine()
+    {
+        if (pantallaNegra != null) pantallaNegra.CrossFadeAlpha(1, 2, false);
+        else Debug.LogWarning("No hay pantalla negra!");
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(nombreEscena);
+    }
+}
