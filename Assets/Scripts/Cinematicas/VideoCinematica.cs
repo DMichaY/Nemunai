37aa9e9f9b0775a7ff3bd1f6aff3f049e2633e38
Micha <davidmichavilayan4@gmail.com>
2025-05-20 23:28:28 +0200

    Ajuste cinematicas para inicio de escenas

---
+++ Assets/Scripts/Cinematicas/VideoCinematica.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoCinematica : MonoBehaviour
+{
+    [Header("Referencias")]
+    public VideoPlayer videoPlayer;               // Componente VideoPlayer
+    public GameObject pantallaVideo;              // UI que muestra el v�deo (con RawImage)
+    public GameObject juegoCompleto;              // Padre del gameplay (personaje, c�mara, etc.)
+    public Camera camaraReferencia;               // C�mara que, si se desactiva, cierra la carta
+
+    [Header("Gameplay")]
+    public float segundosAntesDeActivarGameplay = 1f;
+
+    private bool videoPausado = false;
+
+    void Start()
+    {
+        juegoCompleto.SetActive(false);
+        videoPlayer.loopPointReached += AlTerminarVideo;
+        videoPlayer.Play();
+
+        StartCoroutine(ActivarJuegoAntesDeFin());
+    }
+
+    void Update()
+    {
+
+        if (videoPlayer.isPlaying && Input.GetKeyDown(KeyCode.Escape))
+        {
+            videoPlayer.Pause();
+            videoPausado = true;
+        }
+        else if (videoPausado && Input.GetKeyDown(KeyCode.Escape))
+        {
+            videoPlayer.Play();
+            videoPausado = false;
+        }
+    }
+
+    // Funci�n p�blica para reanudar el v�deo y los controles
+    public void ReanudarVideo()
+    {
+        if (videoPausado)
+        {
+            videoPlayer.Play();
+            videoPausado = false;
+        }
+
+        // Activar controles
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+
+        // Activar juego completo
+        juegoCompleto.SetActive(true);
+    }
+
+    IEnumerator ActivarJuegoAntesDeFin()
+    {
+        double tiempoActivacion = videoPlayer.clip.length - segundosAntesDeActivarGameplay;
+
+        while (videoPlayer.time < tiempoActivacion)
+        {
+            yield return null;
+        }
+
+        juegoCompleto.SetActive(true);
+    }
+
+    void AlTerminarVideo(VideoPlayer vp)
+    {
+        pantallaVideo.SetActive(false);
+    }
+}
